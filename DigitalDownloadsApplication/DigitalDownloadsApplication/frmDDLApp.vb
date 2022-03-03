337ababfd3405a3c4a781c5094d76e5facc9d05e
MGAlysium <98758694+MGAlysium@users.noreply.github.com>
2022-03-03 08:56:14 -0600

    Add files via upload

---
+++ DigitalDownloadsApplication/DigitalDownloadsApplication/frmDDLApp.vb
@@ -0,0 +1,33 @@
+﻿Public Class frmDDLApp
+
+    Const price As Double = 3.5
+
+    Private Sub frmDDLApp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lbl99Cents.Text = price.ToString("C") + " per Download"
+    End Sub
+
+    Private Sub btnCalcCost_Click(sender As Object, e As EventArgs) Handles btnCalcCost.Click
+        Dim inVal As Double = Convert.ToDouble(tbxInput.Text)
+        If inVal < 999 And inVal > 0 Then
+            inVal = inVal * price
+            lblFinalCost.Text = inVal.ToString("C")
+            lblFinalCost.Visible = True
+            btnClear.Enabled = True
+        Else
+            lblFinalCost.Text = "Invalid"
+            lblFinalCost.Visible = True
+            btnClear.Enabled = True
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblFinalCost.Text = ""
+        lblFinalCost.Visible = False
+        tbxInput.Text = ""
+        btnClear.Enabled = False
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+End Class
