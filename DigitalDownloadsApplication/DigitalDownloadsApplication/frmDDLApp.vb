f39bc342a15a04cc11e508b75e1c2da9f83e6750
MGAlysium <98758694+MGAlysium@users.noreply.github.com>
2022-03-03 09:04:21 -0600

    Add files via upload

--- DigitalDownloadsApplication/DigitalDownloadsApplication/frmDDLApp.vb
+++ DigitalDownloadsApplication/DigitalDownloadsApplication/frmDDLApp.vb
@@ -6,6 +6,13 @@
         lbl99Cents.Text = price.ToString("C") + " per Download"
     End Sub
 
+    Private Sub tbxInput_TextChanged(sender As Object, e As EventArgs) Handles tbxInput.TextChanged
+        btnCalcCost.Enabled = True
+        If tbxInput.Text = "" Then
+            btnClear.Enabled = False
+        End If
+    End Sub
+
     Private Sub btnCalcCost_Click(sender As Object, e As EventArgs) Handles btnCalcCost.Click
         Dim inVal As Double = Convert.ToDouble(tbxInput.Text)
         If inVal < 999 And inVal > 0 Then
@@ -13,10 +20,12 @@
             lblFinalCost.Text = inVal.ToString("C")
             lblFinalCost.Visible = True
             btnClear.Enabled = True
+            btnCalcCost.Enabled = False
         Else
             lblFinalCost.Text = "Invalid"
             lblFinalCost.Visible = True
             btnClear.Enabled = True
+            btnCalcCost.Enabled = False
         End If
     End Sub
 
@@ -25,6 +34,7 @@
         lblFinalCost.Visible = False
         tbxInput.Text = ""
         btnClear.Enabled = False
+        btnCalcCost.Enabled = True
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
