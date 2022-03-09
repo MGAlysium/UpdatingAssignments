2c6553904f6ff81b58133964d31559b34b7dc4a4
MGAlysium <98758694+MGAlysium@users.noreply.github.com>
2022-03-09 08:45:16 -0600

    Add files via upload

---
+++ PokerV1/PokerV1/frmPoker.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPoker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmPoker))
+        Me.picDeck = New System.Windows.Forms.PictureBox()
+        Me.picTable = New System.Windows.Forms.PictureBox()
+        Me.btnDeal = New System.Windows.Forms.Button()
+        Me.lblTester = New System.Windows.Forms.Label()
+        CType(Me.picDeck, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTable, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picDeck
+        '
+        Me.picDeck.Image = Global.PokerV1.My.Resources.Resources.Card_Back
+        Me.picDeck.Location = New System.Drawing.Point(255, 117)
+        Me.picDeck.Name = "picDeck"
+        Me.picDeck.Size = New System.Drawing.Size(45, 63)
+        Me.picDeck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picDeck.TabIndex = 1
+        Me.picDeck.TabStop = False
+        '
+        'picTable
+        '
+        Me.picTable.Image = Global.PokerV1.My.Resources.Resources.Poker_Table
+        Me.picTable.Location = New System.Drawing.Point(13, 13)
+        Me.picTable.Name = "picTable"
+        Me.picTable.Size = New System.Drawing.Size(603, 344)
+        Me.picTable.TabIndex = 0
+        Me.picTable.TabStop = False
+        '
+        'btnDeal
+        '
+        Me.btnDeal.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnDeal.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnDeal.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDeal.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnDeal.Location = New System.Drawing.Point(255, 52)
+        Me.btnDeal.Name = "btnDeal"
+        Me.btnDeal.Size = New System.Drawing.Size(116, 40)
+        Me.btnDeal.TabIndex = 2
+        Me.btnDeal.Text = "Deal"
+        Me.btnDeal.UseVisualStyleBackColor = False
+        '
+        'lblTester
+        '
+        Me.lblTester.AutoSize = True
+        Me.lblTester.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.lblTester.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTester.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblTester.Location = New System.Drawing.Point(255, 217)
+        Me.lblTester.Name = "lblTester"
+        Me.lblTester.Size = New System.Drawing.Size(88, 19)
+        Me.lblTester.TabIndex = 3
+        Me.lblTester.Text = "Test Text"
+        '
+        'frmPoker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(627, 364)
+        Me.Controls.Add(Me.lblTester)
+        Me.Controls.Add(Me.btnDeal)
+        Me.Controls.Add(Me.picDeck)
+        Me.Controls.Add(Me.picTable)
+        Me.ForeColor = System.Drawing.Color.Teal
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmPoker"
+        Me.Text = "Poker"
+        CType(Me.picDeck, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTable, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents picTable As System.Windows.Forms.PictureBox
+    Friend WithEvents picDeck As System.Windows.Forms.PictureBox
+    Friend WithEvents btnDeal As System.Windows.Forms.Button
+    Friend WithEvents lblTester As System.Windows.Forms.Label
+
+End Class
---
+++ PokerV1/PokerV1/frmPoker.vb
@@ -0,0 +1,85 @@
+﻿Public Class frmPoker
+
+    Dim deck() As Integer = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+        49, 50, 51}
+    Dim playerCards(1) As Integer
+    Dim enemy1Cards(1) As Integer
+    Dim enemy2Cards(1) As Integer
+    Dim enemy3Cards(1) As Integer
+    Dim dealerCards(4) As Integer
+    Dim dealStage As Integer = 0
+    Dim randNum As Integer
+    Dim randFinal As Integer
+
+    Private Sub btnDeal_Click(sender As Object, e As EventArgs) Handles btnDeal.Click
+        If dealStage = 0 Then
+            deck = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+                15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+                32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+                49, 50, 51}
+            Randomize()
+            randNum = CInt(Int(51 * Rnd()))
+            randFinal = randNum
+            playerCards(0) = deck(randFinal)
+            deck(randFinal) = 52
+            randFinal = WhileLoop()
+            playerCards(1) = deck(randFinal)
+            deck(randFinal) = 52
+            randFinal = WhileLoop()
+            enemy1Cards(0) = deck(randFinal)
+            deck(randFinal) = 52
+            randFinal = WhileLoop()
+            enemy1Cards(1) = deck(randFinal)
+            deck(randFinal) = 52
+            randFinal = WhileLoop()
+            enemy2Cards(0) = deck(randFinal)
+            deck(randFinal) = 52
+            randFinal = WhileLoop()
+            enemy2Cards(1) = deck(randFinal)
+            deck(randFinal) = 52
+            randFinal = WhileLoop()
+            enemy3Cards(0) = deck(randFinal)
+            deck(randFinal) = 52
+            randFinal = WhileLoop()
+            enemy3Cards(1) = deck(randFinal)
+            deck(randFinal) = 52
+            dealStage = 1
+        ElseIf dealStage = 1 Then
+            randFinal = WhileLoop()
+            dealerCards(0) = deck(randFinal)
+            deck(randFinal) = 52
+        ElseIf dealStage = 1 Then
+            randFinal = WhileLoop()
+            dealerCards(1) = deck(randFinal)
+            deck(randFinal) = 52
+        ElseIf dealStage = 1 Then
+            randFinal = WhileLoop()
+            dealerCards(2) = deck(randFinal)
+            deck(randFinal) = 52
+        ElseIf dealStage = 1 Then
+            randFinal = WhileLoop()
+            dealerCards(3) = deck(randFinal)
+            deck(randFinal) = 52
+        ElseIf dealStage = 1 Then
+            randFinal = WhileLoop()
+            dealerCards(4) = deck(randFinal)
+            deck(randFinal) = 52
+            lblTester.Text = playerCards(0).ToString() + ", " + playerCards(1).ToString() + vbNewLine +
+            enemy1Cards(0).ToString() + ", " + enemy1Cards(1).ToString() + vbNewLine +
+            enemy2Cards(0).ToString() + ", " + enemy2Cards(1).ToString() + vbNewLine +
+            enemy3Cards(0).ToString() + ", " + enemy3Cards(1).ToString() + vbNewLine +
+            dealerCards(0).ToString() + ", " + dealerCards(1).ToString() + ", " + dealerCards(2).ToString() +
+            ", " + dealerCards(3).ToString() + ", " + dealerCards(4).ToString()
+        End If
+    End Sub
+
+    Function WhileLoop() As Integer
+        randNum = CInt(Int(51 * Rnd()))
+        While deck(randNum) = 52
+            randNum = CInt(Int(51 * Rnd()))
+        End While
+        Return randNum
+    End Function
+End Class
